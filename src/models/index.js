bf7019fa787551fdf618ecfa85691bb85dd41929
Dan Schultz <slifty@gmail.com>
2017-12-18 13:33:03 -0500

    Fixed config storage
    Yarn wasn't copying the config json, and frankly it was silly to have
    yet another config file.  Moved it over to the main shared config.

--- src/models/index.js
+++ src/models/index.js
@@ -2,14 +2,15 @@ import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
+import { SEQUELIZE_CONFIG } from '../shared/config'
 
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require('../config/config.json')[env]
-
+const config = SEQUELIZE_CONFIG[env]
 const db = {}
 let sequelize = {}
 dotenv.config()
+
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,3 +1,17 @@
 export const WEB_PORT = process.env.PORT || 8800
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'Bloop App'
+export const SEQUELIZE_CONFIG = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    logging: false,
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    logging: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    logging: false,
+  },
+}
