bf5c545afe13d3dfe3c9c53d914e5474b5e105b8
Dan Schultz <slifty@gmail.com>
2017-12-27 10:41:06 -0500

    Sequelize timeout issue
    There appears to be a timeout issue in the Sequelize ORM right now as
    discussed in https://github.com/sequelize/sequelize/issues/7884
    
    This commit adjusts the configuration so that we're less likely to face the
    issue.

--- src/shared/config.js
+++ src/shared/config.js
@@ -5,13 +5,34 @@ export const SEQUELIZE_CONFIG = {
   development: {
     use_env_variable: 'DATABASE_URL',
     logging: false,
+    pool: {
+      max: 5,
+      min: 1,
+      idle: 20000,
+      evict: 20000,
+      acquire: 0,
+    },
   },
   test: {
     use_env_variable: 'DATABASE_URL',
     logging: false,
+    pool: {
+      max: 5,
+      min: 1,
+      idle: 20000,
+      evict: 20000,
+      acquire: 0,
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     logging: false,
+    pool: {
+      max: 5,
+      min: 1,
+      idle: 20000,
+      evict: 20000,
+      acquire: 0,
+    },
   },
 }
